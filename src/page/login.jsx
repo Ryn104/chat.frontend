de79b460dc0569528cfe394335e529ce38bf4af1
Ryn104 <riyananggacuy@gmail.com>
2025-02-21 21:40:24 +0700

    test

--- src/page/login.jsx
+++ src/page/login.jsx
@@ -54,7 +54,7 @@ const Log = () => {
             <div className="xl:flex xl:justify-between xl:min-h-screen bg">
                 <div className="card w-full xl:max-w-[48vw] shadow-2xl backdrop-blur-md backdrop-brightness-100 rounded-none">
                     <form className="card-body" onSubmit={handleSubmit}>
-                        <div className="grid place-content-center h-[100vh] xl:h-full">
+                        <div className="grid place-content-center h-[100vh] xl:h-full ">
                             <div className="logo gap-5 mb-10">
                                 <div className="xl:w-[5vw] w-20">
                                     <img src={photos.logo} alt="Logo" />
@@ -62,14 +62,14 @@ const Log = () => {
                                 <h1 className="text-[13vw] xl:text-[4vw] font-bold xl:font-bold text-gray-600">SENT</h1>
                             </div>
                             {error && <div className="text-red-500">{error}</div>} {/* Tampilkan error jika ada */}
-                            <div className="form-control">
+                            <div className="form-control xl:ml-[20vw]">
                                 <label className="label">
                                     <span className="label-text text-gray-800 font-semibold">Email</span>
                                 </label>
                                 <input
                                     type="email"
                                     placeholder="email"
-                                    className="input input-bordered xl:w-[17vw] xl:h-[4vh] bg-gray-200 xl:text-[0.8vw] text-gray-700"
+                                    className="input input-bordered xl:h-[4vh] bg-gray-200 text-gray-700"
                                     required
                                     value={email}
                                     onChange={(e) => setEmail(e.target.value)} // Update state email
@@ -82,7 +82,7 @@ const Log = () => {
                                 <input
                                     type="password"
                                     placeholder="password"
-                                    className="input input-bordered xl:w-[17vw] xl:h-[4vh] bg-gray-200 xl:text-[0.8vw]"
+                                    className="input input-bordered xl:w-[10vw] xl:h-[4vh] bg-gray-200 xl:text-[0.8vw]"
                                     required
                                     value={password}
                                     onChange={(e) => setPassword(e.target.value)} // Update state password
